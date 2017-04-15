49783f7fa7baf637dbd38346c1ee5815cb235787
Jim Hall <jim.hall@solarmedia.com>
2017-04-15 17:58:23 -0400

    Setup gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    concat = require('gulp-concat');
+
+var jsSources = [
+    'development/js/perfmatters.js'
+];
 
 gulp.task('log', function() {
     gutil.log('Website Optimization');
+});
+
+gulp.task('js', function() {
+    gulp.src(jsSources)
+        .pipe(concat('perfmatters.js'))
+        .pipe(gulp.dest('production/js'))
 });
