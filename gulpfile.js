9d1394085726ce521cc90a59407339dfe2c043ed
Jim Hall <jim.hall@solarmedia.com>
2017-04-15 21:29:56 -0400

    Finish gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,85 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    concat = require('gulp-concat');
+    connect = require('gulp-connect'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    htmlmin = require('gulp-htmlmin'),
+    cssmin = require('gulp-clean-css'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    imageresize = require('gulp-image-resize'),
+    imagemin = require('gulp-imagemin'),
+    pngcrush = require('imagemin-pngcrush');
 
-var jsSources = [
-    'development/js/perfmatters.js'
+var jsSources = ['development/js/perfmatters.js'];
+var cssSources = ['development/css/*.css'];
+var htmlSources = ['development/*.html'];
+var imageSources = [
+    'development/img/**/*.*',
+    'development/views/images/**/*.*'
 ];
-
-gulp.task('log', function() {
-    gutil.log('Website Optimization');
-});
 
 gulp.task('js', function() {
     gulp.src(jsSources)
         .pipe(concat('perfmatters.js'))
+        .pipe(uglify())
         .pipe(gulp.dest('production/js'))
+        .pipe(connect.reload())
 });
+
+gulp.task('css', function() {
+    gulp.src(cssSources)
+        .pipe(cssmin())
+        .pipe(gulp.dest('production/css'))
+        .pipe(connect.reload())
+});
+
+gulp.task('html', function() {
+    gulp.src(htmlSources)
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('production'))
+        .pipe(connect.reload())
+});
+
+var resizeImageTasks = [];
+[120, 240, 360, 480, 640, 760].forEach(function(size) {
+   var resizeImageTask = 'resize_' + size;
+
+    gulp.task(resizeImageTask, function() {
+        gulp.src(imageSources)
+            .pipe(imageresize({
+                imageMagick: true,
+                width: size,
+                crop: false,
+                upscale: false,
+                quality: 0.5
+            }))
+            .pipe(imagemin({
+                progressive: true,
+                use: [pngcrush()]
+            }))
+            .pipe(rename(function(path) {
+                path.basename += '_' + size;
+            }))
+            .pipe(gulp.dest('production/img'))
+    });
+    resizeImageTasks.push(resizeImageTask);
+});
+
+
+gulp.task('watch', function() {
+    gulp.watch(jsSources, ['js']);
+    gulp.watch(htmlSources, ['html']);
+    gulp.watch(cssSources, ['css']);
+    gulp.watch(imageSources, ['images']);
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        root: 'production',
+        livereload: true
+    });
+});
+
+gulp.task('resize_images', resizeImageTasks);
+gulp.task('default', ['js', 'css', 'html', 'connect', 'watch']);
